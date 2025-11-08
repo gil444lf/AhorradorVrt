39910dfdad41b783ed709a69e77f2204bcdb5bca
giraldosebastian32999 <giraldosebastian329@gmail.com>
2025-11-08 01:46:50 -0500

    Remove Banco class methods and constructor

--- src/Banco.java
+++ src/Banco.java
@@ -5,31 +5,4 @@ public class Banco {
     private String nombre;
     private List<Cliente> clientes;
 
-    public Banco(String nombre) {
-        this.nombre = nombre;
-        this.clientes = new ArrayList<>();
-    }
-
-    public void agregarCliente(Cliente cliente) {
-        clientes.add(cliente);
-    }
-
-    public List<Cliente> getClientes() {
-        return clientes;
-    }
-
-    public int totalCuentas() {
-        return clientes.stream().mapToInt(c -> c.getCuentas().size()).sum();
-    }
-
-    public double totalSaldo() {
-        return clientes.stream()
-                .flatMap(c -> c.getCuentas().stream())
-                .mapToDouble(Cuenta::getSaldo)
-                .sum();
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-}
+  
